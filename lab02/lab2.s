258f69e62fa64c89e77fee87ed87ec29bf115677
irfanm96 <e15138@ce.pdn.ac.lk>
2018-10-20 21:32:54 +0530

    completed the lab 2 task

--- lab02/lab2.s
+++ lab02/lab2.s
@@ -15,6 +15,10 @@ main:
 	@ load i to r0
 	@ load j to r1
 	@load sum to r5
+	
+	mov r0,#1 // i=0
+	mov r1,#5 //j=5
+	mov r5,#0 // sum=0
 
 	
 	@ Write YOUR CODE HERE
@@ -22,8 +26,10 @@ main:
 	@	Sum = 0;
 	@	for (i=0;i<10;i++){
 	@			for(j=5;j<15;j++){
-	@				if(i+j<10) sum+=i*2
-	@				else sum+=(i&j);	
+	@				if(i+j<10)
+	@				sum+=i*2
+	@				else 
+	@				sum+=(i&j);	
 	@			}	
 	@	} 
 	@ Put final sum to r5
@@ -31,22 +37,35 @@ main:
 
 	@ ---------------------
 
+	loop1:  cmp r0,#10 // 
+	        bge exit  // i>=10
+	        mov r1,#5  // j=5
+	loop2:	cmp r1,#15  
+	        bge incrementloop1 // j>=15
+	          	
+		add r2,r0,r1 // i+j
+		 cmp r2,#10  // i+j<10
+		 bge else  // i+j >=10
+ 		 add r3,r0,r0 // 2*i
+		 add r5,r5,r3 // sum+=(2*i)
+		 b incrementloop2
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	else : and r2,r0,r1 // i & j
+	       add r5,r5,r2 // sum+= (i & j)
+	       	 
+		   
+    incrementloop2: add r1,r1,#1 // j++
+	                b loop2
+	                
+	  incrementloop1 :	add r0,r0,#1  //i++
+					b loop1
+		               
+
 	@ ---------------------
 	
 	
-	@ load aguments and print
+ exit:	@ load aguments and print
+    @mov r5,r0  
 	ldr r0, =format
 	mov r1, r5
 	bl printf
